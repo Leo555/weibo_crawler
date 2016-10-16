2359a93c709614105ba5e79429c79255078afc1e
Leo.Li <lizhen55521@gmail.com>
2016-10-16 15:51:00 +0800

    update

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Leo on 2016/10/1.
+ */
+
+var loginMsg = {
+    userName: 'lizhen_go@163.com',
+    password: 'qq.2726762.'
+}
+
+exports.loginMsg = loginMsg;
---
+++ test.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Leo on 2016/10/1.
+ */
+
+var Request = require('request');
+var site = "http://weibo.com/3952070245";
+
+function fetch
+Request.get({
+    uri: site, headers: {
+        'User-Agent': 'google'
+    }
+}, function (err, response, body) {
+    if (err) {
+        console.log("访问" + site + "失败")
+        console.log(err);
+    }
+    else {
+        console.log("访问" + site + "完成")
+        var match = body.match(/\d+\.\d+\.\d+\.\d+/g);
+
+        console.log(body);
+    }
+});
